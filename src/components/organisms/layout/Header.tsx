e148a1aafd42cc86944ac5dd60d77144c30343c0
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-06-30 09:32:07 +0900

    ヘッダー作成中 chakraUIはレスポンシブデザインにも対応しやすい

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,5 +1,18 @@
+import { Flex, Heading } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const Header: VFC = memo(() => {
-  return <div style={{ height: "50px", backgroundColor: "teal" }}></div>;
+  return (
+    <Flex
+      as="nav"
+      bg="teal.500"
+      color="gray.50"
+      align="center"
+      justify="space-between"
+    >
+      <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+        ユーザー管理アプリ
+      </Heading>
+    </Flex>
+  );
 });
