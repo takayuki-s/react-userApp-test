bffab3ec516cb7ccb9551b405d8a655a6e26715b
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-07-03 23:09:06 +0900

    ログインボタンをコンポーネント化

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -9,6 +9,8 @@ import {
 } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
+
 export const Login: VFC = memo(() => {
   return (
     <Flex align="center" justify="center" height="100vh">
@@ -19,9 +21,7 @@ export const Login: VFC = memo(() => {
         <Divider my={4} />
         <Stack spacing={6} py={4} px={10}>
           <Input placeholder="ユーザーID" />
-          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
-            ログイン
-          </Button>
+          <PrimaryButton>ログイン</PrimaryButton>
         </Stack>
       </Box>
     </Flex>
