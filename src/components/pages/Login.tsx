74cc47d89a959a9de0591005c49d8d1245f40358
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-07-03 20:39:54 +0900

    ログイン画面雛形

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,15 @@
+import { Box, Button, Divider, Flex, Heading, Input } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const Login: VFC = memo(() => {
-  return <p>ログインページです</p>;
+  return (
+    <Flex>
+      <Box>
+        <Heading as="h1">ユーザー管理アプリ</Heading>
+        <Divider />
+        <Input placeholder="ユーザーID" />
+        <Button>ログイン</Button>
+      </Box>
+    </Flex>
+  );
 });
