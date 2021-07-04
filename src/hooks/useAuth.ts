7f47de9c238f5e2e57c80a0fc26af53409b6b9e1
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-07-04 10:22:57 +0900

    loading画面準備 finallyはes2015だと未対応なので、ts.configでes2018に変える

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { User } from "../types/api/user";
 
 export const useAuth = () => {
   const history = useHistory();
+
+  const [loading, setLoading] = useState(false);
   const login = useCallback(
     (id: string) => {
+      setLoading(true);
       axios
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
@@ -16,9 +19,10 @@ export const useAuth = () => {
             alert("ユーザーが見つかりません");
           }
         })
-        .catch(() => alert("ログインできません"));
+        .catch(() => alert("ログインできません"))
+        .finally(() => setLoading(false));
     },
     [history]
   );
-  return { login };
+  return { login, loading };
 };
