100c4984590d927ae377bca00c6083cb1ed6857a
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-07-06 06:47:51 +0900

    ユーザー一覧ページにAPIで取得したusersを表示

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,18 +1,33 @@
-import { memo, VFC } from "react";
-import { Wrap, WrapItem } from "@chakra-ui/react";
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { memo, useEffect, VFC } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
 
 import { UserCard } from "../organisms/user/userCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+  useEffect(() => getUsers(), []);
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://source.unsplash.com/random"
-          userName="てけ"
-          fullName="teke"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id}>
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
